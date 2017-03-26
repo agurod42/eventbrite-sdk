c032e244deef980ca9aa52c188ea25cdd263ba60
“Agustín <agurz@icloud.com>
2017-03-26 14:02:06 -0300

    event class refactor

--- src/entity/event.js
+++ src/entity/event.js
@@ -2,34 +2,52 @@ var api = require('../api');
 
 const entityPath = '/event';
 
-module.exports = {
+function Event(data) {
+    for (var i in data) {
+        this[i] = data[i];
+    }
+}
 
-    all: function(cb) {
-        api.restAPICall('POST', '/organizer_list_events', { id: require('../eventbrite-sdk').getOrganizerId() }, function(err, res) {
-            if (err) {
-                cb(err, null);
-            }
-            else {
-                var events = [];
+// class methods
 
-                for (var i in res.events) {
-                    events.push(res.events[i].event);
-                };
-                
-                cb(null, events);
-            }
-        });
-    },
+Event.prototype.attendees = function(cb) {
+    api.restAPICall('POST', entityPath + '_list_attendees', { id: this.id }, function (err, res) {
 
-    get: function(id, cb) {
-        api.restAPICall('POST', entityPath + '_get', { id: id }, function(err, res) {
-            if (err) {
-                cb(err, null);
-            }
-            else {
-                cb(null, res.event);
-            }
-        });
-    },
-    
+    });
 };
+
+// static methods
+
+Event.all = function(cb) {
+    api.restAPICall('POST', '/organizer_list_events', { id: require('../eventbrite-sdk').getOrganizerId() }, function (err, res) {
+        if (err) {
+            cb(err, null);
+        }
+        else {
+            var events = [];
+
+            for (var i in res.events) {
+                var event = new Event(res.events[i].event);
+                events.push(event);
+            };
+            
+            cb(null, events);
+        }
+    });
+};
+
+Event.get = function(id, cb) {
+    api.restAPICall('POST', entityPath + '_get', { id: id }, function (err, res) {
+        if (err) {
+            cb(err, null);
+        }
+        else {
+            var event = new Event(res.event);
+            cb(null, event);
+        }
+    });
+};
+
+// exports
+
+module.exports = Event;
