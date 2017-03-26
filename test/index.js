f3d0431f8f4cbbc4eb39e3d653b883da23ea95bd
“Agustín <agurz@icloud.com>
2017-03-26 14:13:19 -0300

    tests code quality improved

--- test/index.js
+++ test/index.js
@@ -5,18 +5,20 @@ var Eventbrite = require('../index');
 
 require('dotenv').config();
 
+const EVENT_ID = 24706151765;
+
 Eventbrite.configure({
     appKey: process.env.EVENTBRITE_APP_KEY,
     userKey: process.env.EVENTBRITE_USER_KEY,
     organizerId: process.env.EVENTBRITE_ORGANIZER_ID
 });
 
-describe('event', function() {
+describe('event', function () {
 
     this.timeout(4000);
 
     it('all', function(done) {
-        Eventbrite.event.all(function(err, events) {
+        Eventbrite.event.all(function (err, events) {
             expect(err).to.be.null;
             expect(events).to.be.a('array');
             expect(events[0].id).to.be.a('number');
@@ -24,8 +26,8 @@ describe('event', function() {
         });
     });
 
-    it('get', function(done) {
-        Eventbrite.event.get(24706151765, function(err, event) {
+    it('get', function (done) {
+        Eventbrite.event.get(EVENT_ID, function (err, event) {
             expect(err).to.be.null;
             expect(event).to.be.a('object');
             expect(event.id).to.be.a('number');
